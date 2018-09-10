7948146f1c0e57178565af344497ad4d71308c9c
Dalton Strahan <daltonstrahan101@gmail.com>
2018-09-10 15:38:42 -0500

    Added number 2

--- GitHello.java
+++ GitHello.java
@@ -2,7 +2,7 @@
 
 public class GitHello {
     public static void main(string[] args){
-        System.out.println("Hello Git");
+        System.out.println("Hello Git2");
         System.exit(0);
     }
 }
